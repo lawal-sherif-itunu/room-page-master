af1d691218ac863adf994fcb5d2641db765b1c99
lawal-sherif-itunu <63552329+lawal-sherif-itunu@users.noreply.github.com>
2024-10-31 23:15:21 +0100

    css review
    css work through

--- sandbox.js
+++ sandbox.js
@@ -12,3 +12,7 @@ close.addEventListener("click", () => {
     navBar.style.display = "none"; // Show the navigation bar
     navLogo.style.display = "block"; // Hide the navigation logo
   });
+
+const nextDom = document.querySelector(".next-desktop");
+const prevDom = document.querySelector(".previous-desktop");
+const secOne = document.querySelector(".section-one");
